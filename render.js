95b47ccce2540503ac0621b5ff3d08e4b3cc693e
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-11-09 16:00:04 +0200

    customized frontend.

---
+++ render.js
@@ -0,0 +1,9 @@
+const funnyBtn = document.getElementById('funnyBtn');
+funnyBtn.onclick = () => {
+  alert('Btn: Hey, it tickles me!');
+};
+
+const windowsErrBtn = document.getElementById('windowsError');
+windowsErrBtn.onclick = () => {
+  alert("Nothing's wrong, just wanted to tell you to have a great day. :D");
+};
